a96ee8e2af06cb972aa6e77c1aa3bf87d76f33ea
Eliot Robson <eliot.robson24@gmail.com>
2023-11-18 22:59:10 -0600

    Renamed class

--- tests/test_ecfr.py
+++ tests/test_ecfr.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 from ecfr.ecfr import cli
 
 
-class TestWeather:
+class TestECFR:
     def test_cli(self) -> None:
         runner = CliRunner()
         result = runner.invoke(cli, ["--help"])
